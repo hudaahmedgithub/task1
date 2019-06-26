691dc8b36204f53cf4223fcaeeaf609aeb3a27c1
huda ahmed <mhudaahmed@gmail.com>
2019-06-26 03:18:07 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,161 @@
+$(document).ready(function(){
+    
+    
+    // Show Navbar when Click on icon
+    $('i.icon').click(function(){
+        $('.nav-list').slideToggle()
+    });
+    
+    // when scroll body .. sticky navbar
+    $(window).scroll(function(){
+        var sc = $(this).scrollTop();
+        if(sc > 50){  // Show Sticky Navbar
+            $('header').addClass('sticky');
+        }
+        else {
+            $('header').removeClass('sticky');
+        }
+        
+        if( sc > 2500) {
+            $('.time').countTo(); // Run Count To Plugin
+            $(window).off('scroll');
+        }
+        
+        if ( sc > 50) { 
+            // Fade The Scroll Top Btn
+            
+            $('.scrollTop').fadeIn();
+            
+        } else {
+            
+            $('.scrollTop').fadeOut();
+            
+        }
+        
+    });
+    
+    // Portfolio Buttons Change bg 
+    $('.buttons button').click(function(){
+        
+        /*$(this).addClass('active_btn').siblings().removeClass('active_btn');*/
+        
+        var CSSFilter = $(this).attr('id');
+        
+        if( CSSFilter === 'all' ) {
+            $('.images > div ').fadeIn();
+        } else {
+            $('.images > div ').fadeOut();
+            $('.images').contents().filter('.' + CSSFilter).fadeIn();
+        }
+        
+    });
+ 
+    
+  
+    
+     $('.formm li').click(function(){
+        
+        /*$(this).addClass('active_btn').siblings().removeClass('active_btn');*/
+        
+        var CSSFilter = $(this).attr('id');
+            $('.right > div ').fadeOut();
+            $('.right').contents().filter('.' + CSSFilter).fadeIn();
+        }
+        
+    });
+    // owl carousel >> Team Section
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+  
+    
+    // Choose Panel 
+    $('.c_panel li').click(function(){
+        $(this).addClass('active_btn').siblings().removeClass('active_btn');
+        
+        var p = $(this).data('role');
+        $('.conten > div ').hide();
+        $('.conten').contents().filter('#' + p).fadeIn();
+    }
+    );
+    
+    $('.panel li').click(function(){
+        
+        $(this).addClass('active_btn').siblings().removeClass('active_btn');
+        
+        var p = $(this).data('id');
+        $('.content > div ').hide();
+        $('.content').contents().filter('#' + p).fadeIn();
+    }
+   });
+    
+    // popup video in choose section
+    $('.pop').magnificPopup({
+        type: 'iframe'
+    });
+    
+    // slick slider on pleasure section
+    $('.slick').slick({
+        nextArrow : false,
+        prevArrow : false
+    });
+    
+    //
+    
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+
+    // Scroll to Top
+    $('.scrollTop').click(function(){
+        $('body,html').animate({
+            scrollTop : 0
+        },500);
+    });
+    
+    // smooth Scroll 
+    $('.list a').click(function(){
+        
+        $('body,html').animate({
+            scrollTop : $($(this).attr('href')).offset().top - 80
+        });
+        
+    }); 
+});
